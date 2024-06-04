31757d93c4f4b4804b03508a704b53ec28a246cf
abdelmalek <abdelmalekmaskri18@gmail.com>
2024-06-04 23:05:55 +0100

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -85,8 +85,15 @@ document.querySelectorAll('.js-add-to-cart')
         
       }
 
-      
-      console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+
     });
 
 
