090a9c9e61319b8aea6391bd13bb426924b4e9b7
abdelmalek <abdelmalekmaskri18@gmail.com>
2024-06-04 22:59:56 +0100

    make add to crt button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach((product) =>{
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart"
+    data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>
@@ -58,3 +59,35 @@ console.log(productHTML);
 
 document.querySelector('.js-products-grid')
   .innerHTML = productHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button)=> {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+
+      cart.forEach((item) =>{
+          if(productId === item.productId){
+            matchingItem = item;
+          }
+      });
+
+      if(matchingItem){
+        matchingItem.quantity +=1;
+      }
+      else{
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+        
+      }
+
+      
+      console.log(cart);
+    });
+
+
+});
