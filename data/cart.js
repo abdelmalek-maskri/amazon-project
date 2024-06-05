959311451c2cc512e82cff147cef35bda13a3a5e
abdelmalek <abdelmalekmaskri18@gmail.com>
2024-06-05 19:20:26 +0100

    save cart in localStorage()

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,15 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 },{
   productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity: 1
 }];
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -28,6 +33,7 @@ export function addToCart(productId){
         quantity: Number(productQuantity) 
       });
     }
+    saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -39,4 +45,6 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
