638a95057d2e5dbc8fb0576ced4719e3443d9024
abdelmalek <abdelmalekmaskri18@gmail.com>
2024-06-10 23:09:56 +0100

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,12 @@
 export let cart = JSON.parse(localStorage.getItem('cart')) || 
     [{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
-        quantity: 2
+        quantity: 2,
+        deliveryOptionId: '1' 
     }, {
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '2'
     }];
 
 
@@ -29,9 +31,11 @@ export function addToCart(productId){
     }else{
         cart.push({
             productId: productId,
-            quantity: Number(quantity)
+            quantity: Number(quantity),
+            deliveryOptionId: '1'
         });
     }
+    
     saveToStorage();
     
 }
@@ -42,7 +46,27 @@ export function removeFromCart(productId) {
     saveToStorage();
 }
 
+export function calculateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  
+  return cartQuantity;
+}
 
+
+export function updateQuantity(productId, newQuantity){
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if(cartItem.productId === productId){
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.quantity = newQuantity;
+  
+  saveToStorage();
+} 
 
 
 // Function to validate product IDs
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,10 @@
 import {products} from '../data/products.js';
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
+updateCartQuantity();
+
 products.forEach((productItem) =>{
     productsHTML +=`
     <div class="product-container">
@@ -62,11 +64,7 @@ document.querySelector('.products-grid')
 
 
 function updateCartQuantity(){
-    let cartQuantity = 0;
-
-    cart.forEach((cartItem) =>{
-        cartQuantity+= cartItem.quantity;
-    });
+    const cartQuantity = calculateCartQuantity();
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
@@ -103,9 +101,6 @@ document.querySelectorAll('.js-add-to-cart')
             updateCartQuantity();
 
             applyCSS(productId, addedMessageTimeouts);
-            
-            
-            
-        });
+      });
 });
 
