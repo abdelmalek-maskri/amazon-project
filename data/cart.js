d939db00afaa01be23dfee5c61517e4557f9a839
abdelmalek <abdelmalekmaskri18@gmail.com>
2024-06-11 19:25:42 +0100

    move orderSummary into its own file

--- data/cart.js
+++ data/cart.js
@@ -68,36 +68,17 @@ export function updateQuantity(productId, newQuantity){
   saveToStorage();
 } 
 
+export function updateDeliveryOption(productId, newDeliveryOptionId){
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingItem = cartItem;
+    }
+  });
+  if(!matchingItem){
+    return;
+  }
+  matchingItem.deliveryOptionId = newDeliveryOptionId;
 
-// Function to validate product IDs
-// function isValidProductId(productId) {
-//     return typeof productId === 'string' && productId.length === 36; // Assuming UUID format
-// }
-
-// // Load cart from localStorage or initialize it with default values
-// let storedCart = JSON.parse(localStorage.getItem('cart'));
-
-// // Check if storedCart is valid
-// if (!Array.isArray(storedCart)) {
-//     storedCart = null;
-// }
-
-// // Validate and clean the stored cart data if it exists
-// if (storedCart) {
-//     storedCart = storedCart.filter(item =>
-//         isValidProductId(item.productId) && Number.isInteger(item.quantity) && item.quantity > 0
-//     );
-// }
-
-// // If the stored cart is invalid or empty, initialize with default values
-// export let cart = storedCart && storedCart.length > 0 ? storedCart : [
-//     {
-//         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-//         quantity: 2,
-//     },
-//     {
-//         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-//         quantity: 1
-//     }
-// ];
- 
+  saveToStorage();
+}
